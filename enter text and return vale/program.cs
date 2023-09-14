f633e82d75152484ea1f371fc9e47f4c3d86eb55
DevvyyXYZ <132696257+devvyyxyz@users.noreply.github.com>
2023-09-14 13:19:08 +0100

    Create program.cs

---
+++ enter text and return vale/program.cs
@@ -0,0 +1,8 @@
+// Type ysome text and press enter
+Console.WriteLine("Enter some text:");
+
+// Create a string variable and get user input from the keyboard and store it in the variable which wil be returned later
+string UserInputText = Console.ReadLine();
+
+// Print the value of the variable (UserInputText), which will display the input value
+Console.WriteLine("You entered: " + UserInputText);
